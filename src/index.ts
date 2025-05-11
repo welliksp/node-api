2611f383975874a44a888f50df25251d97d82632
Wellik Perroni <welliksp@gmail.com>
2025-05-11 16:03:49 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+
+const server = http.createServer(app);
+
+server.listen(8080, () =>{
+
+    console.log('Server is running on port 8080');
+});
+
+const MONGO_URL = 'mongodb+srv://nodeapi:KsvHv8rayCBNkvwt@cluster0.fk50uto.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connected');
+}
+);
